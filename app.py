f992071239ac36d871573ea9d2294d1fe5672aad
Guilherme Lacerda <lacerda@poli.ufrj.br>
2019-06-16 18:28:52 -0300

    aa

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
-def before_request():
-    app.jinja_env.cache = {}
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello'
+    return render_template('templates/i.html')
+
+@app.route('/teste')
+def home2():
+    return 'teste'
 
 if __name__ == '__main__':
-    app.before_request(before_request)
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
