89c4ad08ed9b72a88e857bc55660bbc5b7c5f474
Guilherme Lacerda <lacerda@poli.ufrj.br>
2019-06-22 13:50:49 -0300

    back

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('i.html')
 
 @app.route('/teste')
-def home2():
-    return 'teste'
+def home_2():
+    print('teste')
 
+    
 if __name__ == '__main__':
     app.run()
--- game.py
+++ game.py
@@ -40,28 +40,26 @@ import random
 # criando o mapa do jogo
 t=[]  
 s=[]
-for i in range(1,7):
+for i in range(1,3):
 	for j in range(1,9):
 		t.append([i,j])
 # t.append(s)
 # print(list(t))
 
-game = {'pos1': [[1, 1],[1, 2],[2, 1]],
-'pos2': [[2, 2],[3, 1],[3, 2]],
-'pos3': [[4, 1],[4, 2],[5, 2]],
-'pos4': [[5, 1],[6, 1],[6, 2]],
-'pos5': [[1, 3],[1, 4],[2, 4]],
-'pos6': [[2, 3],[3, 3],[3, 4]],
-'pos7': [[4, 3],[4, 4],[5, 3]],
-'pos8': [[5, 4],[6, 3],[6, 4]],
-'pos9': [[1, 5],[1, 6],[2, 5]],
-'pos10':[[2, 6],[3, 5],[3, 6]],
-'pos11' :[[4, 5],[4, 6],[5, 6]],
-'pos12' :[[5, 5],[6, 5],[6, 6]],
-'pos13' :[[1, 7],[1, 8],[2, 8]],
-'pos14' :[[2, 7],[3, 7],[3, 8]],
-'pos15' :[[4, 7],[4, 8],[5, 7]],
-'pos16':[[5, 8],[6, 7],[6, 8]]}
+game = {
+'pos1': [[1, 1],[1, 2]],
+'pos2': [[1, 3],[1, 4]],
+'pos3': [[1, 5],[1, 6]],
+'pos4': [[1, 7],[1, 8]],
+'pos5': [[2, 1],[2, 2]],
+'pos6': [[2, 3],[2, 4]],
+'pos7': [[2, 5],[2, 6]],
+'pos8': [[2, 7],[2, 8]],
+'pos9': [[3, 1],[3, 2]],
+'pos10': [[3, 3],[3, 4]],
+'pos11': [[3, 5],[3, 6]],
+'pos12': [[3, 7],[3, 8]]
+}
 
 # def getKeysByValue(dict, value):
 #     listOfKeys = list()
@@ -73,21 +71,17 @@ game = {'pos1': [[1, 1],[1, 2],[2, 1]],
 
 initial_pos = {
 	'pos1': 'lacerda',
-'pos2': 'Mozert',
-'pos3': 'Terezinha',
-'pos4': 'arthur',
-'pos5': 'pedro',
-'pos6': 'reb',
-'pos7': 'migs',
-'pos8': 'dona vera',
-'pos9': 'kradeu',
-'pos10': 'john',
-'pos11' : 'rapha',
-'pos12' : 'comp1',
-'pos13' : 'comp2',
-'pos14' : 'comp3',
-'pos15' : 'comp4',
-'pos16': 'comp5'
+'pos2': 'arthur',
+'pos3': 'beto',
+'pos4': 'kradeu',
+'pos5': 'john',
+'pos6': 'logato',
+'pos7': 'grey',
+'pos8': 'girafa',
+'pos9': 'bloris',
+'pos10': 'bruno',
+'pos11' : 'pablo',
+'pos12' : 'be',
 }
 
 g = dict()
